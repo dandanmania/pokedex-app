a0426f61d786f99257ec21d0eb5cdf8653827777
dandanmania <danielkimus@gmail.com>
2022-03-29 14:18:01 -0700

    Add for loop that prints name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 let pokemonList = [
-    {name: 'bulbasaur', pokedexNo: 1, height: 0.7, types: ['grass', 'poison']},
-    {name: 'charmander', pokedexNo: 4, height: 0.6, types: ['fire']},
-    {name: 'squirtle', pokedexNo: 7,height: 0.5, types: ['water']}
+    {name: 'Bulbasaur', pokedexNo: 1, height: 0.7, types: ['grass', 'poison']},
+    {name: 'Charmander', pokedexNo: 4, height: 0.6, types: ['fire']},
+    {name: 'Squirtle', pokedexNo: 7,height: 0.5, types: ['water']}
 ];
+
+for (i = 0; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + ' (Height: ' + pokemonList[i].height + ' m) ')
+    if (pokemonList[i].height > 0.6) {
+        document.write('- Wow, that\'s big! <br>')
+    }
+    else {
+        document.write('<br>')
+    }
+}
