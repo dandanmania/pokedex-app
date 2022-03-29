2394811860b0991e5697e5beaa69c6f1e4dd04a1
dandanmania <danielkimus@gmail.com>
2022-03-28 17:27:49 -0700

    Add array with 3 pokemon objects

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'bulbasaur', pokedexNo: 1, height: 0.7, types: ['grass', 'poison']},
+    {name: 'charmander', pokedexNo: 4, height: 0.6, types: ['fire']},
+    {name: 'squirtle', pokedexNo: 7,height: 0.5, types: ['water']}
+];
