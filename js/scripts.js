e3f2e937a41d6637b336ee36996844f92ea013c7
dandanmania <danielkimus@gmail.com>
2022-04-01 18:51:04 -0700

    Add modal to display pokemon details (Task 1.8 Finished)

--- js/scripts.js
+++ js/scripts.js
@@ -30,8 +30,38 @@ let pokemonRepository = (function() {
 
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function() {
-            console.log(pokemon);
+            //Makes Modal
+            let modalContainer = document.querySelector('#modal-container');
+            modalContainer.innerHTML = '';
+            let modal = document.createElement('div');
+            modal.classList.add('modal');
+
+            //Makes Close Button
+            let closeButtonElement = document.createElement('button');
+            closeButtonElement.classList.add('modal-close');
+            closeButtonElement.innerText = "Close";
+            closeButtonElement.addEventListener('click', hideDetails);
+    
+            //Makes Content in Modal
+            let titleElement = document.createElement('h1');
+            titleElement.innerText = pokemon.name;
+            let imageElement = document.createElement('img');
+            imageElement.src = pokemon.imageUrl;
+            let contentElement = document.createElement('p');
+            contentElement.innerText = 'Height: ' + pokemon.height;
+    
+            modal.appendChild(closeButtonElement);
+            modal.appendChild(titleElement);
+            modal.appendChild(imageElement);
+            modal.appendChild(contentElement);
+            modalContainer.appendChild(modal);
+            modalContainer.classList.add('is-visible')
         });
+    }
+
+    function hideDetails() {
+        let modalContainer = document.querySelector('#modal-container');
+        modalContainer.classList.remove('is-visible');
     }
 
     function loadList() {
@@ -62,6 +92,21 @@ let pokemonRepository = (function() {
             console.error(e);
         });
     };
+
+    window.addEventListener('keydown', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+            hideDetails();
+        }
+    });
+
+    window.addEventListener('click', (e) => {
+        let modalContainer = document.querySelector('#modal-container');
+        let target = e.target;
+        if (target === modalContainer) {
+            hideDetails();
+        }
+    })
 
     return {
         add,
