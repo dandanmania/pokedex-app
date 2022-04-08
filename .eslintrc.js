67fb0258b40d8f762dcc799a3d3a103dc2ae8377
dandanmania <danielkimus@gmail.com>
2022-04-07 17:18:25 -0700

    Add eslint file

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+/* eslint-disable quotes */
+/* eslint-disable no-undef */
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": ["error", "single"]
+    }
+}
